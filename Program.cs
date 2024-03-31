fa3e15a6882c09244f96cb4c54a7c3fd45734c47
Toph33r <165564452+Toph33r@users.noreply.github.com>
2024-03-31 20:49:24 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ASSIGNMENT_KO_2
+{
+    internal class Program
+    {//Lauron, Clarence Christopher L.
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num % 15 == 0)
+                {
+                    Console.WriteLine("Hello,Goodbye!");
+                }
+                else if (num % 3 == 0) {
+                    Console.WriteLine("Hello!");
+                 
+                }else if (num % 5 == 0) { Console.WriteLine("Goodbye!"); }
+                else { Console.WriteLine(num); }
+            }   
+        }
+    }
+}
